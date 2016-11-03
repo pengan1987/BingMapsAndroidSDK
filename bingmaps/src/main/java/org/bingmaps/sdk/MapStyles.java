1f56469b4c986a04a71c4f252f3f72293b02a228
Andy Zhou <andy.zhou@fandine.com>
2016-11-03 13:16:56 -0700

    Add new maps styles in V8 SDK

--- bingmaps/src/main/java/org/bingmaps/sdk/MapStyles.java
+++ bingmaps/src/main/java/org/bingmaps/sdk/MapStyles.java
@@ -1,27 +1,36 @@
 package org.bingmaps.sdk;
 
 /**
- * A class of the standard Map styles. 
+ * A class of the standard Map styles.
+ *
  * @author rbrundritt
  */
 public class MapStyles {
-	/**
-	 * The OrdnanceSurvey map style.
-	 */
-	public static final String OrdnanceSurvey = "Microsoft.Maps.MapTypeId.ordnanceSurvey";
-	
-	/**
-	 * The Road map style.
-	 */
-	public static final String Road = "Microsoft.Maps.MapTypeId.road";
-	
-	/**
-	 * The Aerial map style.
-	 */
-	public static final String Aerial = "Microsoft.Maps.MapTypeId.aerial";
-	
-	/**
-	 * The StreetSide map style.
-	 */
-	public static final String StreetSide = "Microsoft.Maps.MapTypeId.streetside";
+    /**
+     * The OrdnanceSurvey map style.
+     */
+    public static final String OrdnanceSurvey = "Microsoft.Maps.MapTypeId.ordnanceSurvey";
+
+    /**
+     * The Road map style.
+     */
+    public static final String Road = "Microsoft.Maps.MapTypeId.road";
+
+    /**
+     * The Aerial map style.
+     */
+    public static final String Aerial = "Microsoft.Maps.MapTypeId.aerial";
+
+    /**
+     * The StreetSide map style.
+     */
+    public static final String StreetSide = "Microsoft.Maps.MapTypeId.streetside";
+
+    /**
+     * new V8 map styles.
+     */
+    public static final String CanvasDark = "Microsoft.Maps.MapTypeId.canvasDark";
+    public static final String CanvasLight = "Microsoft.Maps.MapTypeId.canvasLight";
+    public static final String Grayscale = "Microsoft.Maps.MapTypeId.grayscale";
+    public static final String Mercator = "Microsoft.Maps.MapTypeId.mercator";
 }
